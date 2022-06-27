cd7f160224a71de8a9f1ae732ea602394534b619
PetitPandaRoux <contact@lsakada.fr>
2022-06-27 21:59:38 +0200

    add dist folder for githubpage deploy

---
+++ dist/_nuxt/static/1656359913/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebplateform\u002F_nuxt\u002Fstatic\u002F1656359913",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fwebplateform\u002F",assetsPath:"\u002Fwebplateform\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/webplateform/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'webplateform',
