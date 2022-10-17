df139077cbf3ebe367610f906255f4e168cc24f8
PetitPandaRoux <contact@lsakada.fr>
2022-10-17 21:27:31 +0200

    revert nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/webplateform/'
-  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'webplateform',
