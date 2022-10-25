b87e0a4f502a1a3c46eae9a63e894690d9b9ea45
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2022-10-25 12:43:53 +0200

    Debut du header

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nav">
+
+        <img src="../src/logo.jpg"/>
+
+        <a href="#" class="contact">Contact us</a>
+        <a href="#">About us</a>
+        <a href="#">Services</a>
+        <a href="#">Home</a>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Header'
+}
+</script>
+
+<style>
+.nav{
+    background-color:white;
+    margin-right: 150px;
+}
+.nav a{
+    float: right;
+    padding: 6.5% 2.5%;
+    font-size: 18px;
+}
+.nav img{
+    float: left;
+    height: 263px;
+    width: 305px;
+}
+.nav .contact{
+    background-color: #FBB811;
+    color: white;
+}
+</style>
