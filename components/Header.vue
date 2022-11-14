9593d6186df9515735a17307e30c5ea20d36de16
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2022-11-14 12:41:25 +0100

    Le header est propre

--- components/Header.vue
+++ components/Header.vue
@@ -3,13 +3,13 @@
 
         <img class="w-[305px] h-[263px] float-left" src="../src/logo.jpg"/>
 
-        <div class="px-10 py-32 float-right text-[18px] space-x-24 font-medium mr-24">
+        <div class="px-36 py-32 float-right text-[18px] space-x-24">
             
             <a href="#" >Home</a>
             <a href="#" >Services</a>
             <a href="#" >About us</a>
-            <div class="bg-[#FBB811] inline">
-                <a href="#" >Contact us</a>
+            <div class="bg-[#FBB811] text-white w-[200px] h-[28px] inline-block">
+                <a href="#" class="px-14">Contact us</a>
             </div>
 
         </div>
