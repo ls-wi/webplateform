7bbb211865c4528881b909358f207827ec64201e
GnomeApprenti <lchakroun@outlook.fr>
2022-12-01 06:59:52 -0500

    added presentation.vue

---
+++ components/Presentation.vue
@@ -0,0 +1,10 @@
+<template>
+    <p class="bg-black overflow-auto text-white text-center font-zilla text-xl pb-10">
+            blablabalablablabalblabalbabalbalablablab   lablablablablba</p>
+</template>
+
+<script>
+export default{
+    name:'Presentation',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Header/>
     <Banner/>
+    <Presentation/>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
 import Banner from '../components/Banner.vue'
+import Presentation from '../components/Presentation.vue';
 export default {
   name: 'IndexPage',
   components:{
     Header,
-    Banner
+    Banner,
+    Presentation,
   }
 }
 </script>
