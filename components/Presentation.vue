72a8663f7047f33858811c1a26ec375875231833
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2022-12-03 13:45:27 +0100

    presentation responsive

--- components/Presentation.vue
+++ components/Presentation.vue
@@ -1,13 +1,20 @@
 <template>
 
     <div>
-        <p class="bg-black overflow-auto px-64 text-white text-center font-zilla text-xl pt-10 pb-10">
-        Nous sommes une Agence digitale basée à Paris ayant pour mission de vous accompagner sur vos projets digitaux, de l’étape d’avant projet web au lancement effectif du site. Tout au long de la durée de vie de nos projets communs, nous mettons tout en oeuvre pour vous proposer des stratégies digitales efficaces et vous permettre d’atteindre vos objectifs. Grâce à notre organisation, nous sommes en capacité de répondre à toutes les demandes de nos clients, peu importe la taille de leur entreprise.</p>
+        <p class="bg-black overflow-auto px-6 lg:px-64 text-white text-center font-zilla lg:text-xl pt-10 pb-10">
+       
+            Nous sommes Web Treasure, une agence digitale basée à Paris. 
+            Notre mission est de vous fournir tous les outils nécessaires pour naviguer vers votre trésor : la réussite de votre projet digital. 
+            De la <span class="bg-[#FBB811] text-black px-1">création de sites web au marketing sur les reseaux sociaux</span>, nous mettons tout en oeuvre pour vous apporter une solution sur mesure efficace, 
+            quelle que soit l'envergure de votre projet. 
+            <br>Web Treasure vous donnera le navire, la carte, les cannons et même la pelle pour naviguer la mer digitale à la recherche du trésor !  
+
+        </p>
         
         <div class="bg-black pb-10">
-            <ul class="grid grid-cols-2 w-4/5 mx-auto bg-black">
-                <li class="contact">Nous contacter</li>
-                <li class="contact">L'équipage</li>
+            <ul class="grid grid-cols-2 lg:w-4/5 mx-auto bg-black text-center">
+                <li class="bg-[#FBB811] duration-500 hover:bg-[#D8DADE] rounded font-zilla my-auto md:mx-32 mx-6 p-3">Nous contacter</li>
+                <li class="bg-[#FBB811] duration-500 hover:bg-[#D8DADE] rounded font-zilla my-auto md:mx-32 mx-6 p-3">L'équipage</li>
             </ul>
         </div>    
     
