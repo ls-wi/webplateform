ec746538a582b2a6e1b90a4ea9b8daef97942c7a
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2023-01-19 13:54:22 +0100

    ecriture du contenu

--- components/ServicesWeb.vue
+++ components/ServicesWeb.vue
@@ -2,14 +2,27 @@
     <div class="p-10 w-3/4 border m-auto my-10 font-zilla">
         <p class="text-center bg-yellow w-min m-auto p-1 text-4xl mb-10">Développement Web</p>
 
-        <div class="flex">
+        <div class="lg:flex h-[1000px]">
 
-            <div class="border w-2/5 mr-10">
-                <p class="text-center text-2xl font-semibold">Créez votre Site Web !</p>
+            <div class="border lg:w-2/5 mr-10">
+                <p class="pt-5 text-center text-2xl font-semibold">Créez votre Site Web avec Nous !</p>
+                <p class="p-10 text-lg text-center">Chez Web Treasure, nous sommes fiers de vous offrir les meilleurs services de développement de sites web pour répondre à tous vos besoins en ligne. Avec notre équipe expérimentée de développeurs et de designers, nous créons des sites web professionnels, attractifs et adaptés à vos objectifs commerciaux. Nous comprenons que chaque entreprise est unique et c'est pourquoi nous offrons des solutions sur mesure pour répondre à vos besoins spécifiques. Faites confiance à Web Treasure pour créer un site web efficace qui vous permettra de développer votre activité en ligne. Contactez-nous dès maintenant pour en savoir plus sur nos services et pour discuter de votre projet de site web.</p>
             </div>
 
-            <div class="border w-3/5">
-                <p class="text-center text-2xl font-semibold">Les étapes</p>
+            <div class="border lg:w-3/5">
+                <p class="text-center text-2xl font-semibold pt-5">Les étapes</p>
+                <ul class="p-5">
+                    <li class="etapes text-yellow">1</li>
+                    <li class="etapes">Définition des objectifs et du public cible</li>
+                    <li class="etapes">Planification du contenu et de la structure du site</li>
+                    <li class="etapes text-yellow">2</li>
+                    <li class="etapes text-yellow">3</li>
+                    <li class="etapes">Conception et deseign du site</li>
+                    <li class="etapes">Développement du site avec des outils poussés</li>
+                    <li class="etapes text-yellow">4</li>
+                    <li class="etapes text-yellow">5</li>
+                    <li class="etapes">Deploiement du site et sécurisation</li>
+                </ul>
             </div>
 
         </div>
