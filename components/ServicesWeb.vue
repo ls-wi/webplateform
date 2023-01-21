4efae202f334e5848283de10317bd730ce2d0aa1
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2023-01-21 14:00:41 +0100

    Better design

--- components/ServicesWeb.vue
+++ components/ServicesWeb.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="p-10 lg:w-3/4 border m-auto my-10 font-zilla">
+    <div class="p-10 lg:w-3/4 m-auto my-10 font-zilla bg-lgrey rounded">
         <p class="text-center bg-yellow w-min m-auto p-1 text-4xl mb-10">Développement Web</p>
 
-        <div class="lg:flex grid grid-cols-1">
+        <div class="lg:flex grid grid-cols-1 ">
 
-            <div class="border lg:w-2/5 lg:mr-10">
-                <p class="pt-5 text-center text-2xl font-semibold">Créez votre Site Web avec Nous !</p>
-                <p class="lg:p-10 p-5 lg:text-lg text-center h-full">Avec notre équipe expérimentée de développeurs et de designers, nous créons des sites web professionnels, attractifs et adaptés à vos objectifs commerciaux. Chaque entreprise est unique, c'est pourquoi nous offrons des solutions sur mesure pour répondre à vos besoins spécifiques. Faites confiance à Web Treasure pour créer un site web efficace qui vous permettra de développer votre activité en ligne. Contactez-nous dès maintenant pour en savoir plus sur nos services et pour discuter de votre projet.</p>
+            <div class=" lg:w-2/5 lg:mr-10 bg-white rounded">
+                <p class="pt-5 text-center text-2xl font-semibold bg-lgrey w-52 m-auto mt-5 p-5 rounded">Créez votre Site Web avec Nous !</p>
+                <p class="lg:p-10 p-5 lg:text-xl text-center lg:mt-12">Avec notre équipe expérimentée de développeurs et de designers, nous créons des <span class="bg-yellow p-1">sites web professionnels, attractifs et adaptés à vos objectifs commerciaux</span>. Chaque entreprise est unique, c'est pourquoi nous offrons des solutions sur mesure pour répondre à vos besoins spécifiques. Faites confiance à Web Treasure pour créer un site web efficace qui vous permettra de <span class="bg-yellow p-1">développer votre activité en ligne</span>. Contactez-nous dès maintenant pour en savoir plus sur nos services et pour discuter de votre projet.</p>
             </div>
 
-            <div class="border lg:w-3/5 lg:mt-0 mt-5">
-                <p class="text-center text-2xl font-semibold pt-5">Les étapes</p>
-                <ul class="pb-16 grid grid-cols-2 h-full">
+            <div class=" lg:w-3/5 lg:mt-0 mt-5 bg-white rounded">
+                <p class="pt-5 text-center text-2xl font-semibold bg-lgrey w-52 m-auto mt-10 p-5 rounded">Les étapes</p>
+                <ul class="pb-28 grid grid-cols-2 h-full">
                     <li class="etapes text-yellow">1</li>
                     <li class="etapes">Idéation</li>
                     <li class="etapes">Conception</li>
