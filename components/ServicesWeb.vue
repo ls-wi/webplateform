46f0cee767a789b98b8afa7994d81e349e4be97f
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2023-01-21 13:38:30 +0100

    Fix "Les étapes"

--- components/ServicesWeb.vue
+++ components/ServicesWeb.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="p-10 w-3/4 border m-auto my-10 font-zilla">
+    <div class="p-10 lg:w-3/4 border m-auto my-10 font-zilla">
         <p class="text-center bg-yellow w-min m-auto p-1 text-4xl mb-10">Développement Web</p>
 
-        <div class="lg:flex h-[1000px]">
+        <div class="lg:flex grid grid-cols-1">
 
-            <div class="border lg:w-2/5 mr-10">
+            <div class="border lg:w-2/5 lg:mr-10">
                 <p class="pt-5 text-center text-2xl font-semibold">Créez votre Site Web avec Nous !</p>
-                <p class="p-10 text-lg text-center">Chez Web Treasure, nous sommes fiers de vous offrir les meilleurs services de développement de sites web pour répondre à tous vos besoins en ligne. Avec notre équipe expérimentée de développeurs et de designers, nous créons des sites web professionnels, attractifs et adaptés à vos objectifs commerciaux. Nous comprenons que chaque entreprise est unique et c'est pourquoi nous offrons des solutions sur mesure pour répondre à vos besoins spécifiques. Faites confiance à Web Treasure pour créer un site web efficace qui vous permettra de développer votre activité en ligne. Contactez-nous dès maintenant pour en savoir plus sur nos services et pour discuter de votre projet de site web.</p>
+                <p class="lg:p-10 p-5 lg:text-lg text-center h-full">Avec notre équipe expérimentée de développeurs et de designers, nous créons des sites web professionnels, attractifs et adaptés à vos objectifs commerciaux. Chaque entreprise est unique, c'est pourquoi nous offrons des solutions sur mesure pour répondre à vos besoins spécifiques. Faites confiance à Web Treasure pour créer un site web efficace qui vous permettra de développer votre activité en ligne. Contactez-nous dès maintenant pour en savoir plus sur nos services et pour discuter de votre projet.</p>
             </div>
 
-            <div class="border lg:w-3/5">
+            <div class="border lg:w-3/5 lg:mt-0 mt-5">
                 <p class="text-center text-2xl font-semibold pt-5">Les étapes</p>
-                <ul class="p-5">
+                <ul class="pb-16 grid grid-cols-2 h-full">
                     <li class="etapes text-yellow">1</li>
-                    <li class="etapes">Définition des objectifs et du public cible</li>
-                    <li class="etapes">Planification du contenu et de la structure du site</li>
+                    <li class="etapes">Idéation</li>
+                    <li class="etapes">Conception</li>
                     <li class="etapes text-yellow">2</li>
                     <li class="etapes text-yellow">3</li>
-                    <li class="etapes">Conception et deseign du site</li>
-                    <li class="etapes">Développement du site avec des outils poussés</li>
+                    <li class="etapes">Design</li>
+                    <li class="etapes">Développement</li>
                     <li class="etapes text-yellow">4</li>
                     <li class="etapes text-yellow">5</li>
-                    <li class="etapes">Deploiement du site et sécurisation</li>
+                    <li class="etapes">Deploiement</li>
                 </ul>
             </div>
 
