26d2b4ecf790c58fdc19d5f1eb3e72a3ab6fa180
PetitPandaRoux <contact@lsakada.fr>
2023-02-06 23:13:08 +0100

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'webplateform',
+    title: 'webtreasure',
     htmlAttrs: {
       lang: 'en'
     },
