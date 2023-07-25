9c3cb8952de33936f075713a1a05abd41cd22a96
GnomeApprenti <lchakroun@outlook.fr>
2023-07-25 18:18:13 +0200

    vue3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    colors:{
-      'dgrey': '#333333',
-      'lgrey': '#D8DADE',
-      'yellow': '#FBB811',
-      'white': '#ffffff',
-      'black': '#000000',
-    },
-    extend: {
-      
-      fontFamily: {
-        zilla: ["ZILLA", "cursive"],
+    theme: {
+      colors:{
+        'dgrey': '#333333',
+        'lgrey': '#D8DADE',
+        'yellow': '#FBB811',
+        'white': '#ffffff',
+        'black': '#000000',
       },
-
-      width:{
-        '3/2': '150%'
+      extend: {
+        
+        fontFamily: {
+          zilla: ["ZILLA", "cursive"],
+        },
+  
+        width:{
+          '3/2': '150%'
+        },
+        height:{
+          'same' : 'same-as-width'
+        }
       },
-      height:{
-        'same' : 'same-as-width'
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1190px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
-    screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1190px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    }
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
+  
