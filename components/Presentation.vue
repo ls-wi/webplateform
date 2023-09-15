589b4b0574a37bac2bbba002941430807f49aeb9
GnomeApprenti <lchakroun@outlook.fr>
2023-09-16 01:29:33 +0200

    revert to commit before adding broken karousel

---
+++ components/Presentation.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="bg-lgrey pt-10 md:pt-0">
+        <h1 class="font-zilla text-center text-4xl md:mt-10 w-1/2 m-auto bg-yellow">En avant vers la conquête du trésor numérique !</h1>
+        <p class=" px-6 lg:px-64 text-center text-lg pt-10 pb-20">
+       
+            Nous sommes Web Treasure, une agence digitale basée à Paris. 
+            Notre mission est de vous fournir tous les outils nécessaires pour naviguer vers votre trésor : la réussite de votre projet digital. 
+            De la <span class="bg-[#FBB811] text-black px-1">création de sites web au marketing sur les reseaux sociaux</span>, nous mettons tout en oeuvre pour vous apporter une solution sur mesure efficace, 
+            quelle que soit l'envergure de votre projet. 
+            <br>
+            Web Treasure vous donnera le navire, la carte, les cannons et même la pelle pour naviguer la mer digitale à la recherche du trésor !
+
+        </p>
+        
+        <div class="">
+            <ul class="grid grid-cols-2 lg:w-4/5 mx-auto bg-lgrey text-center pb-3">
+                <a href="#contact" class="bg-dgrey text-white duration-500 hover:bg-yellow hover:text-black rounded font-zilla my-auto md:mx-32 mx-6 p-3 text-xl border-b-4 border-yellow">Nous contacter</a>
+                <a href="#crew" class="bg-dgrey text-white duration-500 hover:bg-yellow hover:text-black rounded font-zilla my-auto md:mx-32 mx-6 p-3 text-xl border-b-4 border-yellow">L'équipage</a>
+                
+            </ul>
+        </div>   
+        
+    
+    </div>
+
+</template>
+
+<script>
+export default{
+    name:'Presentation',
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
   ],
-    theme: {
-      colors:{
-        'dgrey': '#333333',
-        'lgrey': '#D8DADE',
-        'yellow': '#FBB811',
-        'white': '#ffffff',
-        'black': '#000000',
+  theme: {
+    colors:{
+      'dgrey': '#333333',
+      'lgrey': '#D8DADE',
+      'yellow': '#FBB811',
+      'white': '#ffffff',
+      'black': '#000000',
+    },
+    extend: {
+      
+      fontFamily: {
+        zilla: ["ZILLA", "cursive"],
       },
-      extend: {
-        
-        fontFamily: {
-          zilla: ["ZILLA", "cursive"],
-        },
-  
-        width:{
-          '3/2': '150%'
-        },
-        height:{
-          'same' : 'same-as-width'
-        }
+
+      width:{
+        '3/2': '150%'
       },
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1190px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
+      height:{
+        'same' : 'same-as-width'
       }
     },
-    plugins: [],
-  }
-  
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1190px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
